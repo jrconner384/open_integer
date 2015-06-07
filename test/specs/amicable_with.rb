0714e159d8bb9f8a50b944170416cec630224097
Jason Conner <jason.r.conner@gmail.com>
2015-06-07 16:09:52 -0500

    Added first specs for #amicable_with?.

---
+++ test/specs/amicable_with.rb
@@ -0,0 +1,9 @@
+require File.expand_path '../../helpers/helper', __FILE__
+
+describe Integer do
+  it 'never says a number is amicable with itself.' do
+    (2..10_000).each do |n|
+      n.amicable_with?(n).must_equal false
+    end
+  end
+end
