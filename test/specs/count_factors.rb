eb15d5635cc143da59433b07f8931bdc24622fd0
Jason Conner <jason.r.conner@gmail.com>
2015-06-07 22:21:04 -0500

    Adding new specs for count_factors and factor_pairs.

---
+++ test/specs/count_factors.rb
@@ -0,0 +1,14 @@
+require File.expand_path '../../helpers/helper', __FILE__
+
+describe Integer do
+  it 'returns a number equal to n.factors.count' do
+    (2..10_000).each do |n|
+      n.count_factors.must_equal n.factors.count
+    end
+  end
+
+  it 'raises a RuntimeError when called on 1' do
+    err = -> { 1.count_factors }.must_raise RuntimeError
+    err.message.must_match 'Integers less than 2 do not have proper factors.'
+  end
+end
---
+++ test/specs/factor_pairs.rb
@@ -0,0 +1,23 @@
+require File.expand_path '../../helpers/helper', __FILE__
+
+describe Integer do
+  it 'returns the same number of factor pairs as #factors' do
+    (2..10_000).each do |n|
+      n.factor_pairs.count.must_equal n.factors.count
+    end
+  end
+
+  it 'returns a collection of pairs which contains two of each factor' do
+    (2..10_000).each do |n|
+      flat_pairs = n.factor_pairs.to_a.flatten
+      n.factors do |factor|
+        flat_pairs.count(factor).must_equal 2
+      end
+    end
+  end
+
+  it 'raises a RuntimeError when called on 1 and enumerated' do
+    err = -> { 1.factor_pairs.to_a }.must_raise RuntimeError
+    err.message.must_match 'Integers less than 2 do not have proper factors.'
+  end
+end
