394e16ba313e499465c9dd74ab55e29b468a80d7
Jason Conner <jason.r.conner@gmail.com>
2015-06-07 23:20:24 -0500

    Changing spec description to method name. Added two new specs.

--- test/specs/prime_factors.rb
+++ test/specs/prime_factors.rb
@@ -1,9 +1,23 @@
 require File.expand_path '../../helpers/helper', __FILE__
 
-describe Integer do
-  it 'always returns an Enumerator' do
+describe '#prime_factors' do
+  it 'returns an Enumerator if not given a block' do
     (1..10_000).each do |n|
       n.prime_factors.must_be_kind_of Enumerator
+    end
+  end
+
+  it 'yields only factors of n' do
+    (1..10_000).each do |n|
+      n.prime_factors do |factor|
+        factor.factor_of? n
+      end
+    end
+  end
+
+  it 'yields only primes' do
+    (1..10_000).each do |n|
+      n.prime_factors.each :prime?
     end
   end
 end
